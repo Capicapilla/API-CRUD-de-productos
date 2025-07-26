ca1374320127ff15ae63385ddca6d01ceb2b383f
Capicapilla <javier.capicapilla@gmail.com>
2025-07-26 11:21:23 +0200

    check pull request

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productCtrl = require('../controllers/product.controller');
 
+//rutas
+
 router.post('/', productCtrl.createProduct);
 router.get('/', productCtrl.getProducts);
 router.get('/:id', productCtrl.getProductById);
